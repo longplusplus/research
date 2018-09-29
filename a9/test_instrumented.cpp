7b706cd3176de867769552db3f882766db78b953
lujialong <lujialong@baidu.com>
2018-09-29 22:13:31 +0800

    update code

--- a9/test_instrumented.cpp
+++ a9/test_instrumented.cpp
@@ -1,26 +1,26 @@
-#include "instrumented.h"
-#include <iostream>
+#include "functorized.h"
+#include "swap.h"
+#include <vector>
 
 int main() {
+    /*
     instrumented<int> i1;
     instrumented<int> i2;
     i1 = i2;
     for (std::size_t i = instrumented_base::n; i <= instrumented_base::construction; ++i) {
         std::cout << i1.counts[i] << " ";
     }
+    */
 
-    std::cout << std::endl;
-    instrumented<int> array[10241];
+    count_operations(16, 16 * 1024, sort());
+    count_operations(16, 16 * 1024, stable_sort());
 
-    for (std::size_t r = 16; r < 10240; r <<= 1) {
-        instrumented<int>::initialize(r);
-        std::sort(array, array + r);
-        for (std::size_t i = instrumented_base::n; i <= instrumented_base::construction; ++i) {
-            std::cout << i1.counts[i] << " ";
-        }
-
-        std::cout << std::endl;
-    }
+    //std::vector<int> a(100000, 0);
+    //std::vector<int> b(100000, 0);
+    int a = 16;
+    int b = 2010;
+    my_swap_xor(a, b);
+    std::cout << "a = " << a << ", b = " << b;
 
     return 0;
 }
